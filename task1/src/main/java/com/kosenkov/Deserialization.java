442718b87234d636632c293c487af7f330b4f936
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-07-09 12:10:54 +0300

    Добавлена десериализация экземпляров класса Employee

---
+++ task1/src/main/java/com/kosenkov/Deserialization.java
@@ -0,0 +1,20 @@
+package com.kosenkov;
+
+import java.io.*;
+
+/**
+ * @author Kosenkov Ivan
+ * Created by Kosenkov Ivan on 09.07.2020
+ * lesson 1
+ */
+
+public class Deserialization {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\Projects\\technoserv_2020\\task1\\save\\save.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Employee employee1 = (Employee) objectInputStream.readObject();
+
+        System.out.println(employee1);
+    }
+}
