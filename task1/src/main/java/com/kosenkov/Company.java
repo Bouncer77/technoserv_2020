e4b643f346052d93a675c5a3a9b1a1a1b0170d9c
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-07-14 12:56:01 +0300

    Изменен алгоритм поиска сотрудников для перевода в другой отдел, при котором растет средняя зп в двух отделах

--- task1/src/main/java/com/kosenkov/Company.java
+++ task1/src/main/java/com/kosenkov/Company.java
@@ -1,5 +1,6 @@
 package com.kosenkov;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -17,13 +18,66 @@ public class Company {
         this.companyName = companyName;
     }
 
-    public StringBuilder avgSalaryIncreaseWhenTransferringEmployeeToAnotherDepartment() {
+    /*public StringBuilder avgSalaryIncreaseWhenTransferringEmployeeToAnotherDepartment() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Department dep1 : departmentList) {
             for (Department dep2 : departmentList) {
                 if (!dep1.getDepartmentName().equals(dep2.getDepartmentName())) {
                     for (int i = 0; i < dep1.getEmployeeList().size(); i++) {
                         stringBuilder.append(dep1.transferringEmployeeToAnotherDepartment(i, dep2));
+                    }
+                }
+            }
+        }
+        return stringBuilder;
+    }*/
+
+    public StringBuilder avgSalaryIncreaseWhenTransferringEmployeeToAnotherDepartment() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Department dep1 : departmentList) {
+            for (Department dep2 : departmentList) {
+                if (!dep1.getDepartmentName().equals(dep2.getDepartmentName())) {
+
+                    //  Рассматриваем для перевода только сотрудников из отдела с наивысшей средней зп,
+                    //  у которых зарплата ниже или равна средней зп по отделу
+                    //  (! равна средней только в том случае рассматриваем, если у всех сотрудников по отделу одна зп)
+
+                    BigDecimal avgSalaryDep1 = dep1.getAvgSalary();
+                    if (dep1.getAvgSalary().compareTo(dep2.getAvgSalary()) == 1) {
+
+                        List<Employee> employeeListDep1 = dep1.getEmployeeList();
+
+                        // Одного сотрудника в отделе никогда нет смысла переводить
+                        // (Если зп 0.0, то это тоже не повлияет, тк средние зп по отделам должны увеличиться,
+                        // а отрицательные зп недопустимы в рамках бизнес модели)
+                        if (employeeListDep1.size() == 1) {
+                            continue;
+                        }
+                        // Случай при котором все сотрудники получают одинаковую зп
+                        BigDecimal salaryFistEmployee = employeeListDep1.get(0).getSalary();
+                        boolean allEmployeesInDepHaveTheSameSalary = true;
+                        for (int i = 1; i < employeeListDep1.size(); i++) {
+                            // Если хотя бы одна зп не равна, то не у всех сотрудников в отделе одинаковые зарплаты
+                            if (salaryFistEmployee.compareTo(employeeListDep1.get(i).getSalary()) != 0) {
+                                allEmployeesInDepHaveTheSameSalary = false;
+                                break;
+                            }
+                        }
+
+                        // Если у всех сотрудников одинаковые зп
+                        // То имеет смысл переводить всех сотрудников
+                        if (allEmployeesInDepHaveTheSameSalary) {
+                            for (int i = 0; i < employeeListDep1.size(); i++) {
+                                stringBuilder.append(dep1.transferringEmployeeToAnotherDepartment(i, dep2));
+                            }
+                            continue; // переходим к след отделу
+                        }
+
+                        for (int i = 0; i < employeeListDep1.size(); i++) {
+                            if (employeeListDep1.get(i).getSalary().compareTo(avgSalaryDep1) == -1) {
+                                stringBuilder.append(dep1.transferringEmployeeToAnotherDepartment(i, dep2));
+                            }
+                        }
                     }
                 }
             }
